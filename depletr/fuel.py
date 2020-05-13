9281fc527f66713389ef44d877c66c58d6457e70
tjlaboss <travis@ktc-technology.com>
2020-05-12 21:03:33 -0400

    Clean up fuel module

--- depletr/fuel.py
+++ depletr/fuel.py
@@ -1,17 +1,36 @@
 # Fuel
+#
+# Functions related to fuel compositions
 
 
 import numpy as np
-from scipy.optimize import fsolve
 from scipy.constants import eV
 from .nuclides.half_lives import DAY
-from .nuclides.constants import NATURAL_U235
 
 
-ELEMENTS = ("U", "Pu", "Uranium", "Plutonium")
+ELEMENTS = ("U", "Uranium")  # + ("Pu", "Plutonium")
 
 
 def give_me_fuel(element, enrichment, num_nuclides):
+	"""Get a nuclide vector for fresh fuel
+	
+	Parameters:
+	-----------
+	element: str
+		Element name or atomic symbol.
+		Only Uranium is currently supported.
+	
+	enrichment: float; wt%
+		U235 enrichment
+	
+	num_nuclides: int
+		Total number of nuclides to accommodate in the vector
+	
+	Returns:
+	--------
+	np.ndarray(dtype=float, len=num_nuclides)
+		Array with nuclides populated for fresh fuel and 0 for everything else
+	"""
 	assert element in ELEMENTS, "Implemented fuel types: {}".format(ELEMENTS)
 	assert num_nuclides >= 2, "You cannot have fewer than 2 nuclides."
 	fuel_nuclides = np.zeros(num_nuclides)
@@ -21,34 +40,95 @@ def give_me_fuel(element, enrichment, num_nuclides):
 		fuel_nuclides[0] = at235
 		fuel_nuclides[1] = at238
 	else:
-		# Plutonium, mox?
+		# Plutonium, MOX, ...
 		raise NotImplementedError(element)
 	return fuel_nuclides
 
 
 def give_me_fire(watts_per_gram, burnup):
-	"""Get the time to achieve a required burnup"""
+	"""Get the time to achieve a required burnup
+	
+	Parameters:
+	-----------
+	watts_per_gram: float; W/g
+		Power density of the reactor
+	
+	burnup: float; MW-d/kg-HM
+		Desired fuel burnup at EOC
+	
+	Returns:
+	--------
+	float; seconds
+		Time to reach `burnup` with a power density of `watts_per_gram`
+	"""
 	burnup *= DAY*1E3  # convert from MWd/kg to W-s/g
 	return burnup/watts_per_gram
 
 
+def give_me_that_which_i_desire():
+	"""Ooh"""
+	print("https://youtu.be/G1cjHbXdU0s?t=6")
+
+
 def get_fission_rate(megawatts, mev_per_fission=200):
-	"""Get the number of required fissions"""
+	"""Get the number of required fissions
+	
+	Parameters:
+	-----------
+	megawatts: float; MW
+		Total reactor thermal power
+	
+	mev_per_fission: float; MeV/fission
+		Average energy released per fission.
+		TODO: Put this in the Nuclide class and data libraries.
+		[Default: 200]
+	
+	Returns:
+	--------
+	float; MeV/s
+		Fission rate required to achieve the desired power
+	"""
 	mevs = megawatts/eV
 	return mevs/mev_per_fission
 
 
 def wt_to_at_uranium(wt235):
+	"""Convert weight fraction to atom fraction for uranium fuel
+	
+	Parameter:
+	----------
+	wt235: float
+		Weight fraction U235
+	
+	Returns:
+	--------
+	float
+		Atom fraction U235
+	"""
 	assert 0 <= wt235 <= 1, "Weight fraction must be on [0, 1]."
 	wt238 = 1 - wt235
+	# To be rigorous, replace with the actual atomic masses.
 	at238 = wt238/238
 	at235 = wt235/235
 	return at235/(at235 + at238)
 
 
 def at_to_wt_uranium(at235):
+	"""Convert atom fraction to weight fraction for uranium fuel
+
+	Parameter:
+	----------
+	at235: float
+		Atom fraction U235
+
+	Returns:
+	--------
+	float
+		Weight fraction U235
+	"""
 	assert 0 <= at235 <= 1, "Atom fraction must be on [0, 1]."
 	at238 = 1 - at235
+	# To be rigorous, replace with the actual atomic masses.
 	wt238 = at238*238
 	wt235 = at235*235
 	return wt235/(wt235 + wt238)
@@ -59,6 +139,21 @@ def wt_to_at_plutonium(wt239):
 
 
 def at_to_wt_arbitrary(list_of_nuclides, list_of_ats):
+	"""Convert multiple atom fractions to an array of weight fractions
+	
+	Parameters:
+	-----------
+	list_of_nuclides: Iterable of Nuclide
+		Nuclides for which to convert atom fraction to weight fraction
+	
+	list_of_ats: Iterable of float
+		Atom fractions of each nuclide
+	
+	Returns:
+	--------
+	np.ndarray(dtype=float)
+		Array of weight fractions of each nuclide
+	"""
 	num = len(list_of_ats)
 	total_at = sum(list_of_ats)
 	list_of_wts = np.zeros(num)
@@ -66,22 +161,3 @@ def at_to_wt_arbitrary(list_of_nuclides, list_of_ats):
 		list_of_wts[i] = nuc.a*at/total_at
 	list_of_wts /= list_of_wts.sum()
 	return list_of_wts
-
-
-def kinf_mox(n, actinide_dict, data, enrichment=NATURAL_U235):
-	nat235 = wt_to_at_uranium(enrichment)
-	nu_fission = 0
-	absorption = 0
-	for nuc, q in actinide_dict.items():
-		nu_fission += n*q*nuc.nu_sigma_f
-		absorption += n*q*nuc.sigma_a
-	nu_fission += (1 - n)*nat235*data.u235.nu_sigma_f
-	absorption += (1 - n)*nat235*data.u235.sigma_a
-	nu_fission += (1 - n)*(1 - nat235)*data.u238.nu_sigma_f
-	absorption += (1 - n)*(1 - nat235)*data.u238.sigma_a
-	return nu_fission/absorption
-
-
-def solve_kinf_mox_for_n(target_kinf, actinide_dict, data, enrichment=NATURAL_U235, nguess=0.15):
-	for_n = lambda n: kinf_mox(n, actinide_dict, data, enrichment) - target_kinf
-	return fsolve(for_n, nguess)[0]
